1011ea3184d6fe02d6d51ed500c0c4aa662bbf19
Devin Sherry <DevinSherry16@gmail.com>
2022-04-08 18:37:39 +0200

    Exercise11.04

---
+++ Chapter11/Exercise11.04/Content/Input/SuperSideScroller_Player.cpp
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SuperSideScroller_Player.h"
+#include "Components/InputComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+
+ASuperSideScroller_Player::ASuperSideScroller_Player()
+{
+	//Set sprinting to false by default.
+	bIsSprinting = false;
+	//Set our Max Walk Speed to 300.0f
+	GetCharacterMovement()->MaxWalkSpeed = 300.0f;
+}
+
+void ASuperSideScroller_Player::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	UEnhancedInputComponent* EnhancedPlayerInput = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	if(EnhancedPlayerInput)
+	{
+		APlayerController* PlayerController = Cast<APlayerController>(GetController());
+		UEnhancedInputLocalPlayerSubsystem* EnhancedSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem> (PlayerController->GetLocalPlayer());
+		if(EnhancedSubsystem)
+		{
+			EnhancedSubsystem->AddMappingContext(IC_Character, 1);
+		}
+		
+		//Bind pressed action Sprint to your Sprint function
+		EnhancedPlayerInput->BindAction(IA_Sprint, ETriggerEvent::Triggered, this, &ASuperSideScroller_Player::Sprint);
+
+		//Bind released action Sprint to your StopSprinting function
+		EnhancedPlayerInput->BindAction(IA_Sprint, ETriggerEvent::Completed, this, &ASuperSideScroller_Player::StopSprinting);
+
+		//Bind the pressed action Throw to your ThrowProjectile function
+		EnhancedPlayerInput->BindAction(IA_Throw, ETriggerEvent::Triggered, this, &ASuperSideScroller_Player::ThrowProjectile);
+	}
+}
+
+void ASuperSideScroller_Player::Sprint()
+{
+	if(!bIsSprinting)
+	{
+		bIsSprinting = true;
+		GetCharacterMovement()->MaxWalkSpeed = 500.0f;
+	}
+}
+
+void ASuperSideScroller_Player::StopSprinting()
+{
+	if(bIsSprinting)
+	{
+		bIsSprinting = false;
+		GetCharacterMovement()->MaxWalkSpeed = 300.0f;
+	}
+}
+
+void ASuperSideScroller_Player::ThrowProjectile()
+{
+	UE_LOG(LogTemp, Warning, TEXT("THROW PROJECTILE"));
+}
