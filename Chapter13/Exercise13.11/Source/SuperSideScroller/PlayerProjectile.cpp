97d435516da6694fc920013fd8a81b8b5ba466b5
Devin Sherry <DevinSherry16@gmail.com>
2022-04-18 13:12:29 +0200

    Exercise13.11

---
+++ Chapter13/Exercise13.11/Source/SuperSideScroller/PlayerProjectile.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerProjectile.h"
+
+// Sets default values
+APlayerProjectile::APlayerProjectile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void APlayerProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	FVector NormalImpulse, const FHitResult& Hit)
+{
+}
+
