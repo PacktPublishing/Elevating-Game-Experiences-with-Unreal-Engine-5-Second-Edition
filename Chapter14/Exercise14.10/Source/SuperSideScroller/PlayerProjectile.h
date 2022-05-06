350528158e7006d4e2d62957aa4521a1aedd8074
Devin Sherry <DevinSherry16@gmail.com>
2022-05-06 21:32:32 +0200

    Exercise14.10

---
+++ Chapter14/Exercise14.10/Source/SuperSideScroller/PlayerProjectile.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PlayerProjectile.generated.h"
+
+UCLASS()
+class SUPERSIDESCROLLER_API APlayerProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APlayerProjectile();
+
+	UFUNCTION() 
+	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+	void ExplodeProjectile();
+	
+public: 
+
+	//Sphere collision component 
+	UPROPERTY(VisibleDefaultsOnly, Category = Projectile) 
+	class USphereComponent* CollisionComp;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = Sound) 
+	class UAudioComponent* ProjectileMovementSound;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = Projectile) 
+	class UParticleSystemComponent* ProjectileEffect; 
+
+private: 
+
+	//Projectile movement component 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	class UProjectileMovementComponent* ProjectileMovement; 
+
+	//Static mesh component 
+	UPROPERTY(EditAnywhere, Category = Projectile) 
+	class UStaticMeshComponent* MeshComp; 
+
+};
