79bd3a5ce8278d669ae025138b32168c18e64175
Devin Sherry <DevinSherry16@gmail.com>
2022-05-25 19:08:01 +0200

    Exercise15.05

---
+++ Chapter15/Exercise15.05/Source/SuperSideScroller/SuperSideScroller_Controller.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "SuperSideScroller_Controller.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUPERSIDESCROLLER_API ASuperSideScroller_Controller : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
