994d7623ab02b122c5787bff2d5e177dfe0700ba
Gonçalo Marques <goncalo.alex.marx@gmail.com>
2022-08-22 18:30:26 +0100

    Fixed DodgeballCharacter class from Exercise 8.04

--- Chapter08/Exercise8.04/DodgeballCharacter.cpp
+++ Chapter08/Exercise8.04/DodgeballCharacter.cpp
@@ -1,17 +1,20 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "DodgeballCharacter.h"
-#include "HeadMountedDisplayFunctionLibrary.h"
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/InputComponent.h"
-#include "HealthComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/Controller.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+#include "InputAction.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "HealthComponent.h"
 #include "Kismet/KismetSystemLibrary.h"
 #include "DodgeballPlayerController.h"
-
 
 //////////////////////////////////////////////////////////////////////////
 // ADodgeballCharacter
@@ -21,9 +24,8 @@ ADodgeballCharacter::ADodgeballCharacter()
 	// Set size for collision capsule
 	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
 
-	// set our turn rates for input
-	BaseTurnRate = 45.f;
-	BaseLookUpRate = 45.f;
+	// set our turn rate for input
+	TurnRateGamepad = 50.f;
 
 	// Don't rotate when the controller rotates. Let that just affect the camera.
 	bUseControllerRotationPitch = false;
@@ -31,22 +33,22 @@ ADodgeballCharacter::ADodgeballCharacter()
 	bUseControllerRotationRoll = false;
 
 	// Configure character movement
+	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f); // ...at this rotation rate
 
-	// Character moves in the direction of input...	
-	GetCharacterMovement()->bOrientRotationToMovement = true;
-	// ...at this rotation rate
-	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f);
-	GetCharacterMovement()->JumpZVelocity = 600.f;
-	GetCharacterMovement()->AirControl = 0.2f;
+	// Note: For faster iteration times these variables, and many more, can be tweaked in the Character Blueprint
+	// instead of recompiling to adjust them
+	GetCharacterMovement()->JumpZVelocity = 700.f;
+	GetCharacterMovement()->AirControl = 0.35f;
+	GetCharacterMovement()->MaxWalkSpeed = 500.f;
+	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
+	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
 
 	// Create a camera boom (pulls in towards the player if there is a collision)
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
-	// The camera follows at this distance behind the character
 	CameraBoom->TargetArmLength = 900.0f;
-	//The camera looks down at the player
 	CameraBoom->SetRelativeRotation(FRotator(-70.f, 0.f, 0.f));
-	// Rotate the arm based on the controller
 	CameraBoom->bUsePawnControlRotation = false;
 	CameraBoom->bInheritPitch = false;
 	CameraBoom->bInheritYaw = false;
@@ -57,10 +59,10 @@ ADodgeballCharacter::ADodgeballCharacter()
 	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
 	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
 
-	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
-	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
-
 	HealthComponent = CreateDefaultSubobject<UHealthComponent>(TEXT("Health Component"));
+
+	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
+	// are set in the derived blueprint asset named ThirdPersonCharacter (to avoid direct content references in C++)
 }
 
 void ADodgeballCharacter::OnDeath_Implementation()
@@ -80,21 +82,25 @@ void ADodgeballCharacter::SetupPlayerInputComponent(class UInputComponent* Playe
 	// Set up gameplay key bindings
 	check(PlayerInputComponent);
 
-	PlayerInputComponent->BindAxis("MoveForward", this, &ADodgeballCharacter::MoveForward);
-	PlayerInputComponent->BindAxis("MoveRight", this, &ADodgeballCharacter::MoveRight);
-
 	// handle touch devices
 	PlayerInputComponent->BindTouch(IE_Pressed, this, &ADodgeballCharacter::TouchStarted);
 	PlayerInputComponent->BindTouch(IE_Released, this, &ADodgeballCharacter::TouchStopped);
 
-	// VR headset functionality
-	PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &ADodgeballCharacter::OnResetVR);
-}
+	UEnhancedInputComponent* EnhancedPlayerInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	if (EnhancedPlayerInputComponent != nullptr)
+	{
+		APlayerController* PlayerController = Cast<APlayerController>(GetController());
+		if (PlayerController != nullptr)
+		{
+			UEnhancedInputLocalPlayerSubsystem* EnhancedSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
+			if (EnhancedSubsystem != nullptr)
+			{
+				EnhancedSubsystem->AddMappingContext(IC_Character, 1);
+			}
+		}
 
-
-void ADodgeballCharacter::OnResetVR()
-{
-	UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
+		EnhancedPlayerInputComponent->BindAction(IA_Move, ETriggerEvent::Triggered, this, &ADodgeballCharacter::Move);
+	}
 }
 
 void ADodgeballCharacter::TouchStarted(ETouchIndex::Type FingerIndex, FVector Location)
@@ -107,31 +113,26 @@ void ADodgeballCharacter::TouchStopped(ETouchIndex::Type FingerIndex, FVector Lo
 	StopJumping();
 }
 
-void ADodgeballCharacter::MoveForward(float Value)
+void ADodgeballCharacter::Move(const FInputActionValue& Value)
 {
-	if ((Controller != NULL) && (Value != 0.0f))
+	FVector2D InputValue = Value.Get<FVector2D>();
+	if (Controller != nullptr && (InputValue.X != 0.0f || InputValue.Y != 0.0f))
 	{
-		// find out which way is forward
-		const FRotator Rotation = Controller->GetControlRotation();
-		const FRotator YawRotation(0, Rotation.Yaw, 0);
+		const FRotator YawRotation(0, Controller->GetControlRotation().Yaw, 0);
 
-		// get forward vector
-		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-		AddMovementInput(Direction, Value);
-	}
-}
+		if (InputValue.X != 0.0f)
+		{
+			// get right vector 
+			const FVector RightDirection = UKismetMathLibrary::GetRightVector(YawRotation);
+			// add movement in that direction
+			AddMovementInput(RightDirection, InputValue.X);
+		}
 
-void ADodgeballCharacter::MoveRight(float Value)
-{
-	if ( (Controller != NULL) && (Value != 0.0f) )
-	{
-		// find out which way is right
-		const FRotator Rotation = Controller->GetControlRotation();
-		const FRotator YawRotation(0, Rotation.Yaw, 0);
-	
-		// get right vector 
-		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
-		// add movement in that direction
-		AddMovementInput(Direction, Value);
+		if (InputValue.Y != 0.0f)
+		{
+			// get forward vector
+			const FVector ForwardDirection = YawRotation.Vector();
+			AddMovementInput(ForwardDirection, InputValue.Y);
+		}
 	}
 }
