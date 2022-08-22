0d0bdb897ee9b30970492c18120952413e56c648
Gonçalo Marques <goncalo.alex.marx@gmail.com>
2022-08-22 18:18:07 +0100

    Fix DodgeballCharacter class from Exercise 7.04

--- Chapter07/Exercise7.04/DodgeballCharacter.h
+++ Chapter07/Exercise7.04/DodgeballCharacter.h
@@ -1,9 +1,10 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputActionValue.h"
 #include "HealthInterface.h"
 #include "DodgeballCharacter.generated.h"
 
@@ -19,32 +20,23 @@ class ADodgeballCharacter : public ACharacter, public IHealthInterface
 	/** Follow camera */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	class UCameraComponent* FollowCamera;
-
-	class UHealthComponent* HealthComponent;
-
 public:
 	ADodgeballCharacter();
 
 	virtual void OnDeath_Implementation() override;
 
 	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
-	float BaseTurnRate;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Input)
+	float TurnRateGamepad;
 
-	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
-	float BaseLookUpRate;
+	UPROPERTY(EditAnywhere, Category = Input)
+	class UInputMappingContext* IC_Character;
+
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	class UInputAction* IA_Move;
 
 protected:
-
-	/** Resets HMD orientation in VR. */
-	void OnResetVR();
-
-	/** Called for forwards/backward input */
-	void MoveForward(float Value);
-
-	/** Called for side to side input */
-	void MoveRight(float Value);
 
 	/** Handler for when a touch input begins. */
 	void TouchStarted(ETouchIndex::Type FingerIndex, FVector Location);
@@ -52,10 +44,16 @@ protected:
 	/** Handler for when a touch input stops. */
 	void TouchStopped(ETouchIndex::Type FingerIndex, FVector Location);
 
+	void Move(const FInputActionValue& Value);
+
 protected:
 	// APawn interface
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	// End of APawn interface
+
+private:
+
+	class UHealthComponent* HealthComponent;
 
 public:
 	/** Returns CameraBoom subobject **/
@@ -63,3 +61,4 @@ public:
 	/** Returns FollowCamera subobject **/
 	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
 };
+
